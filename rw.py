9ec96ce33a5234c0c5c2710fadfad785fb2d71ec
Jan Erik Gulliksen <jamalmarlone@gmail.com>
2022-10-07 14:41:04 +0200

    Add files via upload

---
+++ rw.py
@@ -0,0 +1,56 @@
+# read and write to file
+# by jamalmarlone@gmail.com 2023-10-07
+# https://pyautogui.readthedocs.io/en/latest/quickstart.html
+import pyautogui
+
+fname = "demofile2" #default filename
+
+def w_function(newtxt):
+    f = open(fname +".txt", "w")
+    f.write(newtxt)
+    f.close()
+
+#open and read the file after the appending:
+def r_function():
+    f = open(fname +".txt", "r")
+    return f.read()
+
+while True:  
+    print("\n*** MENU ***")  
+    print(" 1.  Read file")  
+    print(" 2.  Write to file")  
+    print(" 3.  Set filename")  
+    # print("3. Multiplication of two Numbers")  
+    # print("4. Division of two Numbers")  
+    print(" 5.  Exit")  
+
+    print("\n Current filename: " + fname + ".txt\n")
+    users_choice = int(input("\n Enter your Choice: "))  
+
+    # based on the users choice the relevant method is called
+    if users_choice == 1:  
+        print( "\nRead file\n---\n")  
+        print(r_function())
+        print("\n---\n\n")
+    elif users_choice == 2:  
+        print( "\nWrite to file\n---\n")  
+        oldtxt = r_function()  
+        # newtxt = input(oldtxt)  
+        pyautogui.typewrite(oldtxt)
+        newtxt = input()
+        w_function(newtxt)  
+        #print(r_function())
+        print("\n---\n\n")
+    elif users_choice == 3:  
+            print( "\nSet filename\n---\n")  
+            print(" Old filename: " + fname)
+            fname = input(" New filename: ")
+            f = open(fname +".txt", "a")
+            f.close()
+            print("\n New filename: " + fname)
+ # exit the while loop
+    elif users_choice == 5:  
+        break  
+      
+    else:  
+        print( " Please enter a valid Input from the list")          
